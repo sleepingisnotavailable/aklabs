c51794d3b642a0f11b50ec072acec80b7840082c
sleepingisnotavailable <134231181+sleepingisnotavailable@users.noreply.github.com>
2023-12-15 01:11:02 +0200

    Add files via upload

---
+++ lab4/inc/hello1.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2-Clause
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/ktime.h>
+int show_greeting(uint count);
